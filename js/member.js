8cbe4809fbb040cd4f373771fe8187abdb1b9c61
darkshadotin <tinacoldrain1@gmail.com>
2019-02-15 23:38:24 +0800

    add lightBox

--- js/member.js
+++ js/member.js
@@ -8,12 +8,16 @@ function changeTabs(evt, tabList) {
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
+
     }
     document.getElementById(tabList).style.display = "block";
+    
     evt.currentTarget.className += " active";
   }
   
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
+
+
 
 
