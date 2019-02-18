b0b744429f93a390439dffcd6682d10410454ec5
kyotenkacat <kyotenkacat@gmail.com>
2019-02-18 16:18:55 +0800

    add shopping.js

--- js/shopping.js
+++ js/shopping.js
@@ -93,15 +93,15 @@ function searchBar(){
     var name = document.getElementById('searchName').value;
     if( country != 0 ){
         country = "'" + country + "'";
-    }
+    };
     if( kind != 0 ){
         kind = "'" + kind + "'";
-    }
+    };
     if( flavor == 0){
         var search = "and nation = " + country + " and snackGenre = " + kind + " and snackName like '%" + name + "%'";
     }else{
         var search = "and nation = " + country + " and snackGenre = " + kind + " and " + flavor + "Stars > 0" + " and snackName like '%" + name + "%'";
-    }
+    };
     location.href = 'shopping.php?search=' + search;
 }
 window.addEventListener('load', function (){
