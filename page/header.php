25b7d39b2a4077c5994f5a7342f75987c412211e
darkshadotin <tinacoldrain1@gmail.com>
2019-02-19 21:16:03 +0800

    會員登入註冊

--- page/header.php
+++ page/header.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,16 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
         crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/loginBox.css">
     <link rel="stylesheet" href="../css/header.css">
     <script src="../js/jquery-3.3.1.min.js"></script>
-
+    <script src="../js/header.js" defer></script>
     <title>Document</title>
-
 </head>
 
 <body>
-    <header>
+<header>
         <h1>大零食家</h1>
         <div class="cloud">
             <div class="doc doc--bg2">
@@ -104,7 +101,10 @@
             </div>
         </div>  
     </header>
-    <div id="lightBox-wrap">
+<!-- //-------------------------------------------------------//
+-----------------------       這是燈箱        ------------------ -->
+<!-- //-------------------------------------------------------// -->
+<div id="lightBox-wrap">
         <div id="lightBox">
             <div class="loginLeave">
                 <span id="lightBoxLeave">X</span>
@@ -212,10 +212,6 @@
 
 
     </div>
-    
-    <script src="../js/header.js"></script>
-
-
 
 
 </body>
