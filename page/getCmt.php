b1b0535746280625fedf58913f42abd9db562efc
Shu <zero5405@gmail.com>
2019-02-23 14:39:22 +0800

    0223 showItem Done

--- page/getCmt.php
+++ page/getCmt.php
@@ -22,12 +22,12 @@ if($page==0){//第一次進來 要回傳第0~2的評價 以及要產生多少頁
         $totalPage=ceil($totalRec/3);//總共要產生幾頁''
         
         $pageHTML=
-        '<ul><li class="page-item"><a href="#" id="last" class="page-link"><i class="fas fa-chevron-left"></i></a></li>';
+        '<ul><li class="page-item"><a  id="last" class="page-link" page=""><i class="fas fa-chevron-left"></i></a></li>';
        for($i=1;$i<$totalPage+1;$i++){
-        $pageHTML.='<li class="page-item"><a href="#" class="page-link ">'.$i.'</a></li>';
+        $pageHTML.='<li class="page-item"><a  class="page-link" page="'.$i.'">'.$i.'</a></li>';
        }
             
-        $pageHTML.='<li class="page-item"><a href="#" id="next" class="page-link"><i class="fas fa-chevron-right"></i></a></li>
+        $pageHTML.='<li class="page-item"><a  id="next" class="page-link" page=""><i class="fas fa-chevron-right"></i></a></li>
         </ul>';
         $rsp->pages=$pageHTML;
         // <li class="page-item"><a href="#" class="page-link nowLoc">01</a></li>
@@ -46,7 +46,7 @@ if($page==0){//第一次進來 要回傳第0~2的評價 以及要產生多少頁
             </div>
             <div class="memCol">
                 <div class="memId">
-                    <p>'.$cmt["memId"].'</p><button class="report">...</button>
+                    <p>'.$cmt["memId"].'</p><button class="report" to="cmt" repNo='.$cmt["evaNo"].'>...</button>
                 </div>
                 <div class="star" grad="'.$cmt["goodStar"].'"><img src="../images/rankBoard/starMask.png" alt="星等"></div>
                 <p class="commentCtx">
@@ -82,31 +82,33 @@ if($page==0){//第一次進來 要回傳第0~2的評價 以及要產生多少頁
     while($cmt=$feed->fetch()){
     $cmtsHTML.=
     '<div class="mem">
-        <div class="memPic">
-            <img src="'.$cmt["memPic"].'" alt="會員頭像" class="memImg">
+    <div class="memPic">
+        <img src="'.$cmt["memPic"].'" alt="會員頭像" class="memImg">
+    </div>
+    <div class="memCol">
+        <div class="memId">
+            <p>'.$cmt["memId"].'</p><button class="report" repNo='.$cmt["evaNo"].'>...</button>
         </div>
-        <div class="memCol">
-            <div class="memId">
-                <p>'.$cmt["memId"].'</p><button class="report">...</button>
-            </div>
-            <div class="star" grad="'.$cmt["goodStar"].'"><img src="../images/rankBoard/starMask.png" alt="星等"></div>
-            <p class="commentCtx">
-            '.$cmt["evaCtx"].'
-            </p>
-            <div class="commentBtns">
-                <button class="like"><i class="far fa-thumbs-up"></i>'.$cmt["like"].'</button>
-                <button class="share"><i class="fas fa-share"></i>分享</button>
-                <button class="btnMsg" evaNo='.$cmt["evaNo"].'><i class="fas fa-comment"></i>顯示留言</button>
-            </div>
+        <div class="star" grad="'.$cmt["goodStar"].'"><img src="../images/rankBoard/starMask.png" alt="星等"></div>
+        <p class="commentCtx">
+        '.$cmt["evaCtx"].'
+        </p>
+        <p class="msgTime">留言時間:'.$cmt["evaDate"].'</p>
+        <div class="commentBtns">
+            <button class="like" evaNo='.$cmt["evaNo"].'><i class="far fa-thumbs-up"></i>'.$cmt["like"].'</button>
+            <button class="share"><i class="fas fa-share"></i>分享</button>
+            <button class="btnMsg" evaNo='.$cmt["evaNo"].'><i class="fas fa-comment"></i>顯示留言</button>
+        </div>
 
-            <div class="msgBox">
-                <input type="text" class="msg" name="msg" placeholder="留言......">
-                <input type="submit" class="sendMsg" evaNo='.$cmt["evaNo"].' value="送出">
-            </div>
-            <div class="msgs" id="msgBox'.$cmt["evaNo"].'">
-            </div>
+        <div class="msgBox">
+            <input type="text" class="msg" name="msg" placeholder="留言......">
+            <input type="submit" class="sendMsg" evaNo='.$cmt["evaNo"].' value="送出">
         </div>
-    </div>';
+        <div class="msgs" id="msgBox'.$cmt["evaNo"].'">
+        </div>
+
+    </div>
+</div>';
     }
 
     $rsp->cmtArr =$cmtsHTML;
