7d8fa2aa104c45a96c804e3fbe202264ef0b9e46
Shu <zero5405@gmail.com>
2019-02-25 10:18:06 +0800

    sendEvaOK

--- js/sendEva.js
+++ js/sendEva.js
@@ -2,13 +2,12 @@ function addBtnEva(){
 
     $('.sendEva').click(function(){
         var snackNo=$(this).attr('id');
-        var thisForm = document.forms[`snackNo${snackNo}`];
         
-        var evaCtx=$(`form[name=snackNo${snackNo}] textarea`).val();
-        var sweetStar=$(`form[name=snackNo${snackNo}] input[name=swStar]:checked`).val();
-        var sourStar=$(`form[name=snackNo${snackNo}] input[name=suStar]:checked`).val();
-        var spicyStar=$(`form[name=snackNo${snackNo}] input[name=spStar]:checked`).val();
-        var goodStar=$(`form[name=snackNo${snackNo}] input[name=gdStar]:checked`).val();
+        var evaCtx=$(`tr[name=snackNo${snackNo}] textarea`).val();
+        var sweetStar=$(`tr[name=snackNo${snackNo}] input[name=swStar]:checked`).val();
+        var sourStar=$(`tr[name=snackNo${snackNo}] input[name=suStar]:checked`).val();
+        var spicyStar=$(`tr[name=snackNo${snackNo}] input[name=spStar]:checked`).val();
+        var goodStar=$(`tr[name=snackNo${snackNo}] input[name=gdStar]:checked`).val();
 
         var data_info=`snackNo=${snackNo}&evaCtx=${evaCtx}&goodStar=${goodStar}&sourStar=${sourStar}&sweetStar=${sweetStar}&spicyStar=${spicyStar}`;
         var xhr= new XMLHttpRequest();
@@ -17,7 +16,6 @@ function addBtnEva(){
         xhr.send(data_info);
         xhr.onload=function(){
             alert("感謝您提供的意見，加100分");
-            $(thisForm).height('0');
 
         }
         console.log(data_info);
