7345c8b68feb2c8b2f5d18b407c9663dd16c7394
kyotenkacat <kyotenkacat@gmail.com>
2019-03-01 19:18:39 +0800

    送出評價時更新商品資料/登入燈箱input加border

--- page/sendEva.php
+++ page/sendEva.php
@@ -20,7 +20,15 @@ $point_sql = "update member set memPoint = memPoint + 100 where memNo =:memNo";
 $point = $pdo->prepare( $point_sql );
 $point -> bindParam( ":memNo", $_SESSION["memNo"]);
 $point -> execute();
-  
+
+$sql = "update snack set goodTimes = goodTimes + 1, goodStars = goodStars + :goodStars, sourTimes = sourTimes + 1, sourStars = sourStars + :sourStars, sweetTimes = sweetTimes + 1, sweetStars = sweetStars + :sweetStars, spicyTimes = spicyTimes + 1, spicyStars = spicyStars + :spicyStars where snackNo = :snackNo";
+$snack = $pdo -> prepare($sql);
+$snack -> bindValue( ":snackNo",$_REQUEST["snackNo"] );
+$snack -> bindValue( ":goodStars", $_REQUEST["goodStar"] );
+$snack -> bindValue( ":sourStars", $_REQUEST["sourStar"] );
+$snack -> bindValue( ":sweetStars", $_REQUEST["sweetStar"] );
+$snack -> bindValue( ":spicyStars", $_REQUEST["spicyStar"] );
+$snack -> execute();
 
 }catch(PDOException $e){
   echo $e->getMessage();
