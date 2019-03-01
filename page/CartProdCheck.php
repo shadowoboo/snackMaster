bc15371598687de76dba3871600886cb89fe6439
Shadow <shadowoboo@gmail.com>
2019-03-02 02:04:36 +0800

    cartShow修正 1. 關閉cartShow.js 所有alert 2. 卡關 session，避免有名字沒商品造成購物車顯示異常 3. 即期品 若優惠方案日期異常(過期等)則不給進購物車並移除session

--- page/CartProdCheck.php
+++ page/CartProdCheck.php
@@ -2,7 +2,7 @@
     session_start();
     $errMsg="";
     try {
-        if(isset($_SESSION["snackName"])){ //檢查有沒有 snackNo 存在於 session 之中
+        if(isset($_SESSION["snackName"]) && count($_SESSION["snackName"])>=1){ //檢查有沒有 snackNo 存在於 session 之中
             //如果只有"客製關聯商品"但是沒有"客製箱本身"=>不是真的客製箱=>刪除"客製關聯商品"避免衝突
             if(isset($_SESSION["note"][1])){
                 if(in_array("box",$_SESSION["note"][1])==false && in_array("cus",$_SESSION["note"][1])==true){
