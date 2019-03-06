1f83660d84d240733eae4e5107799baa3d01ad3c
yuanshulu <zero5405@gmail.com>
2019-03-06 19:07:42 +0800

    排行榜修改

--- page/getRankBoard.php
+++ page/getRankBoard.php
@@ -7,23 +7,23 @@
         switch($_REQUEST['rankGenre']){
             case 1:
             $rankGenre='綜合';
-            $sql = "SELECT * FROM `snack` ORDER by `goodStars`/`goodTimes` DESC LIMIT 6";
+            $sql = "SELECT * FROM `snack` ORDER by `goodStars`/`goodTimes` DESC,`goodTimes` DESC LIMIT 6";
             break;
             case 2:
             $rankGenre='餅乾';
-            $sql = "SELECT * FROM `snack` WHERE `snackGenre`='餅乾' ORDER by `goodStars`/`goodTimes` DESC LIMIT 6";
+            $sql = "SELECT * FROM `snack` WHERE `snackGenre`='餅乾' ORDER by `goodStars`/`goodTimes` DESC,`goodTimes` DESC LIMIT 6";
             break;
             case 3:
             $rankGenre='糖果';
-            $sql = "SELECT * FROM `snack` WHERE `snackGenre`='糖果' ORDER by `goodStars`/`goodTimes` DESC LIMIT 6";
+            $sql = "SELECT * FROM `snack` WHERE `snackGenre`='糖果' ORDER by `goodStars`/`goodTimes` DESC,`goodTimes` DESC LIMIT 6";
             break;
             case 4:
             $rankGenre='巧克力';
-            $sql = "SELECT * FROM `snack` WHERE `snackGenre`='巧克力' ORDER by `goodStars`/`goodTimes` DESC LIMIT 6";
+            $sql = "SELECT * FROM `snack` WHERE `snackGenre`='巧克力' ORDER by `goodStars`/`goodTimes` DESC,`goodTimes` DESC LIMIT 6";
             break;
             case 5:
             $rankGenre='洋芋片';
-            $sql = "SELECT * FROM `snack` WHERE `snackGenre`='洋芋片' ORDER by `goodStars`/`goodTimes` DESC LIMIT 6";
+            $sql = "SELECT * FROM `snack` WHERE `snackGenre`='洋芋片' ORDER by `goodStars`/`goodTimes` DESC,`goodTimes` DESC LIMIT 6";
             break;
 
         }
